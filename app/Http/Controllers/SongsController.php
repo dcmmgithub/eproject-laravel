114023403b3d588368d78699e6a38fe87b2787ba
dcmmgithub <qbu2k9@gmail.com>
2025-05-03 22:08:34 +0700

    backend: Songs searching

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Songs;
 use Illuminate\Http\Request;
-
+use Illuminate\Database\Eloquent\Builder;
 class SongsController extends Controller
 {
     /**
@@ -47,5 +47,39 @@ class SongsController extends Controller
     public function destroy(Songs $songs)
     {
         //
+    }
+
+    /**
+     * Search for songs by name.
+     */
+    public function search(Request $request)
+    {
+        $validated = $request->validate([
+            'q' => 'required|string|min:1',
+        ]);
+
+        $searchTerm = $validated['q'];
+
+        $songs = Songs::query()->with(['artist', 'album'])
+            ->where(function (Builder $query) use ($searchTerm) {
+
+                $query->where('songs.name', 'LIKE', "%{$searchTerm}%");
+
+                $query->orWhereHas('artist', function (Builder $artistQuery) use ($searchTerm) {
+                    $artistQuery->where('artists.name', 'LIKE', "%{$searchTerm}%");
+                });
+
+                $query->orWhereHas('album', function (Builder $artistQuery) use ($searchTerm) {
+                    $artistQuery->where('albums.name', 'LIKE', "%{$searchTerm}%");
+                });
+
+            })
+            ->orderBy('name')->get();
+
+        if ($songs->isEmpty()) {
+            return response()->json([],404); // Use 404 Not Found status
+        }
+
+        return response()->json($songs);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/songs/search', [SongsController::class, 'search']);
 Route::apiResource('/songs',SongsController::class);
